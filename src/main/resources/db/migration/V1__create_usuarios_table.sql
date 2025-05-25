3152352c701449d9f82327a191746126e1183063
vpaesi <vitoria.camargo@dbserver.com.br>
2025-05-25 15:53:25 -0300

    feat(US001-cadastro-de-usuario): ajuste na tabela usuarios para corrigir tamanhos de campos e garantir consistência

--- src/main/resources/db/migration/V1__create_usuarios_table.sql
+++ src/main/resources/db/migration/V1__create_usuarios_table.sql
@@ -1,9 +1,16 @@
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
-    nome_completo VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(50) NOT NULL,
+    nome_completo VARCHAR(50) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
-    data_nascimento DATE
+    email VARCHAR(50) NOT NULL UNIQUE,
+    telefone VARCHAR(15) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_de_nascimento DATE,
+	cep VARCHAR(10) NOT NULL,
+	estado VARCHAR(2) NOT NULL,
+	cidade VARCHAR(50) NOT NULL,
+	bairro VARCHAR(50) NOT NULL,
+	logradouro VARCHAR(50) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	complemento VARCHAR(50)
 );
